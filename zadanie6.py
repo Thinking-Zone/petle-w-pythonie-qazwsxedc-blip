c3685ca0c92794adf4728b718fc2b2dffce5589b
qazwsxedc-blip <karolka.dra@gmail.com>
2024-11-08 13:38:31 +0100

    Update zadanie6.py
    qwerstdyfugihiet

---
+++ zadanie6.py
@@ -0,0 +1,2 @@
+#Pętle while powtarzają coś tak długo, jak warunek jest prawdziwy, i sprawdzają warunek tylko na początku
+#Pętle For mogą być używane do powtarzania czegoś dla każdego elementu na liście.
